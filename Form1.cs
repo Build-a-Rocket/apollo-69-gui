0d707549e035972426fed71fd740545b13709102
sharkman5046 <hiradmirhashemi@gmail.com>
2022-11-27 00:52:44 -0600

    Update Form1

--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,6 @@ namespace Apollo69GUI
 {
     public partial class GUI : Form
     {
-
-        Random r;
-
         public GUI()
         {
             InitializeComponent();
@@ -26,29 +23,39 @@ namespace Apollo69GUI
 
             realTimeChart2.SetTitle("Random Data 2");
             realTimeChart2.AddChart("Points", "Number", "Random", SeriesChartType.FastLine);
-
-            r = new Random();
         }
 
-        private void loadFileButton_Click(object sender, EventArgs e)
+        private void OpenFileButton_Click(object sender, EventArgs e)
         {
-            string[] lines = File.ReadAllLines(fileNameBox.Text);
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            string path = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+                FileNameBox.Text = path;
+            }
+            
+            string[] lines = File.ReadAllLines(path);
 
             foreach (string line in lines)
             {
-                outputBox.AppendText(line + "\r\n");
+                string[] data = line.Split(',');
+                
+                realTimeChart1.AddPoint(data[0], data[1], "Random");
+                realTimeChart2.AddPoint(data[2], data[3], "Random");
             }
         }
 
-        private void saveFileButton_Click(object sender, EventArgs e)
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            File.WriteAllText(fileNameBox.Text, inputBox.Text);
+
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void DataPointsBox_TextChanged(object sender, EventArgs e)
         {
-            realTimeChart1.AddPoint(0, r.NextDouble(), "Random");
-            realTimeChart2.AddPoint(0, r.NextDouble(), "Random");
+
         }
     }
 }
